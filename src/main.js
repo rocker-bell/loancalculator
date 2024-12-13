4e7bcfe9f2907436827bba85f132bf960a0a09dc
rocker bell <rockbell8@gmail.com>
2024-12-13 12:38:33 +0100

    Add element selection for js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+let amount = document.getElementById('amount').value;
+let annual_interest = document.getElementById('annual').value;
+let yearly_repayment = document.getElementById('years').value;
+let Zipcode = document.getElementById('Zipcode').value;
+
+let Mounthly_payment = document.getElementById('Payment').value;
+let Total_payment = document.getElementById('Total').value;
+let Total_Interest = document.getElementById('interest').value;
+
